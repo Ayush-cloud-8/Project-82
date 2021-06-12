77cdbbf02598f963d2657ba8ca81e5e07482b14e
Ayush-cloud-8 <83327864+Ayush-cloud-8@users.noreply.github.com>
2021-06-12 15:18:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+var canvas = document.getElementById("my_canvas");
+var ctx = canvas.getContext("2d");
+var mouse_state = " ";
+var width = 2;
+var color = "black";
+var radius = 50;
+
+canvas.addEventListener("mousedown" , down);
+
+function down(e) {
+    mouse_state = "mouse_down";
+    width = document.getElementById("line_width").value;
+    color = document.getElementById("line_color").value;
+    radius = document.getElementById("radius_input").value;
+}
+
+
+canvas.addEventListener("mousemove" , move);
+
+function move(e) {
+    if (mouse_state == "mouse_down") {
+        x = e.clientX - canvas.offsetLeft;
+        y = e.clientY - canvas.offsetTop;
+        console.log("Mouse X = " + x);
+        console.log("Mouse Y = " + y);
+
+        ctx.beginPath();
+        ctx.styleStroke = color;
+        ctx.lineWidth = width;
+        ctx.arc(x , y , radius , 0 , 360);
+        ctx.stroke();
+    }
+}
+
+canvas.addEventListener("mouseup" , up);
+
+function up(e) {
+    mouse_state = "mouse_up";
+}
+
+function clear_canvas() {
+    ctx.clearRect(0 , 0 , canvas.width , canvas.height);
+}
